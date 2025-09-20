53388a6955a5a96b7c4ebe0a0e391bd73f40a1fd
marichemia <marichemia10@gmail.com>
2025-09-20 17:03:38 +0400

    setting up routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import {Register} from './auth/register/register';
+import {Checkout} from './checkout/checkout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "login", component: Login},
+    {path: "register", component: Register},
+    {path: "checkout", component: Checkout},
+
+    {path: '',
+        loadChildren: () => import('./products/products-module').then(m => m.ProductsModule)
+    }, 
+
+    {path: '**', redirectTo: ''}
+
+];
--- src/app/products/products-routing-module.ts
+++ src/app/products/products-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductList } from './product-list/product-list';
+import { ProductDetail } from './product-detail/product-detail';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: ProductList},
+  {path: ':id', component: ProductDetail}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
